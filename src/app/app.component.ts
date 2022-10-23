da3c71674e9888c5f78f942d27a26196f74d6ca7
Bekir Kurt <bekiribra111@gmail.com>
2022-10-23 16:03:38 +0300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { skip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,76 +7,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-
-
-
 export class AppComponent {
 
   title = 'Calculator';
 
   itWorksDisplay: boolean = true;
-  itWorksResult: boolean = true;
 
   display : string = "";
   result : string = "Result";
 
-
-
-  ekleme(number: string):string {
+  ekleme(number: string):void {
+      
     if(this.itWorksDisplay){
       this.itWorksDisplay = false;
-      this.result = "";
-      return "";
+      this.result = number;
     }
     else if(this.result.startsWith(".")){
-      return this.result = "";
+      this.result = "";
     }
-    // else if(this.result.endsWith("0")){
-    //    return "";
-    // }
+    else if(this.result.endsWith(".")&& number == "."){}
+    else if(this.result.includes(".")&&number =="."){}
+    else if(number=="0"&&this.result[1]=="0"){
+      this.result = this.result.substring(0,this.result.length-1);
+      skip;
+    }
+    else if(this.result == "" && number=="."){
+      skip;
+    }
     else{
-      
-      return this.result += number;
+      this.result += number;
     }
-  
   }
 
   islem(operations : string):void {
 
     if(this.display.endsWith("x")||this.display.endsWith("-")||
        this.display.endsWith("+")||this.display.endsWith("÷")||this.display.endsWith("%")){
-
+        this.result += "";
+    }
+    // else if(this.display.startsWith("x")||this.display.startsWith("-")||
+    //    this.display.startsWith("+")||this.display.startsWith("÷")||this.display.startsWith("%")){
+    //     this.result = "";
+    // }
+    else if(this.result == "Result"){
+      this.result = "";
     }
     else{
       this.display += this.result + operations;
       this.result = "";
     }
-
-
-    
   }
   
   allClear():void{
     this.display = "";
     this.result = "";
   }
-  
 
   answer():void {
-    if(this.display.includes('+')){
-        this.display =  (Number(this.display.substring(0,this.display.length-1)) + Number(this.result)).toString();
+    if(this.result.includes("+")||this.result.includes("-")||this.result.includes("x")||this.result.includes("÷")||this.result.includes("%")){
+
     }
-    else if(this.display.includes('-')){
-      this.display =  (Number(this.display.substring(0,this.display.length-1)) - Number(this.result)).toString();
-    }
-    else if(this.display.includes('x')){
-      this.display =  (Number(this.display.substring(0,this.display.length-1)) * Number(this.result)).toString();
-    }
-    else if(this.display.includes('÷')){
-      this.display =  (Number(this.display.substring(0,this.display.length-1)) / Number(this.result)).toString();
-    }
-    else if(this.display.includes('%')){
-      this.display =  (Number(this.display.substring(0,this.display.length-1)) % Number(this.result)).toString();
+    else{
+      if(this.display.includes('+')){
+        this.display = (Number(this.display.substring(0,this.display.length-1)) + Number(this.result)).toString();
+      }
+      else if(this.display.includes('-')){
+        this.display =  (Number(this.display.substring(0,this.display.length-1)) - Number(this.result)).toString();
+      }
+      else if(this.display.includes('x')){
+        this.display =  (Number(this.display.substring(0,this.display.length-1)) * Number(this.result)).toString();
+      }
+      else if(this.display.includes('÷')){
+        this.display =  (Number(this.display.substring(0,this.display.length-1)) / Number(this.result)).toString();
+      }
+      else if(this.display.includes('%')){
+        this.display =  (Number(this.display.substring(0,this.display.length-1)) % Number(this.result)).toString();
+      }
     }
     this.result = "";
   }
