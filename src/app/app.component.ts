d3f8ec61218c5a162c7b828500ea7ab1c3da59ba
Bekir Kurt <bekiribra111@gmail.com>
2022-10-28 16:35:20 +0300

    0.3000000000000004 hatasi 0.12300 sondakileri atma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { skip } from 'rxjs';
+import { single, skip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,8 @@ export class AppComponent {
 
   title = 'Calculator';
   itWorksDisplay: boolean = true;
+  dotCheck: boolean = true;
+  arele:number = 0;
 
   display : string = "";
   result : string = "Result";
@@ -76,19 +78,34 @@ export class AppComponent {
   }
 
   answer():void {
+     // 0.2 +0.1 = 0.30000000000000004 hatasi
+     if(this.display.includes(".") || this.result.includes(".")){
+      if(this.display.length > this.result.length){
+        this.arele = this.display.length - 3;
+      }
+      else{
+        this.arele = this.result.length-2; 
+      }
+     }
+     else{
+        this.arele = 0;
+     }
+     
+
     if(this.result.includes("+")||this.result.includes("-")||this.result.includes("x")
     ||this.result.includes("÷")||this.result.includes("%")||this.result.includes("√")||this.result.includes("^")){
 
     }
     else{
       if(this.display.includes('+')){
-        this.display = (Number(this.display.substring(0,this.display.length-1)) + Number(this.result)).toString();
+        this.display =  ((Number(this.display.substring(0,this.display.length-1)) + Number(this.result)).toFixed(this.arele)).toString();
+        //this.display = (Number(this.display.substring(0,this.display.length-1)) + Number(this.result)).toString();
       }
       else if(this.display.includes('-')){
-        this.display =  (Number(this.display.substring(0,this.display.length-1)) - Number(this.result)).toString();
+        this.display =  ((Number(this.display.substring(0,this.display.length-1)) - Number(this.result)).toFixed(this.arele)).toString();
       }
       else if(this.display.includes('x')){
-        this.display =  (Number(this.display.substring(0,this.display.length-1)) * Number(this.result)).toString();
+        this.display =  ((Number(this.display.substring(0,this.display.length-1)) * Number(this.result)).toFixed(this.arele+3)).toString();
       }
       else if(this.display.includes('÷')){
         this.display =  (Number(this.display.substring(0,this.display.length-1)) / Number(this.result)).toString();
@@ -105,7 +122,37 @@ export class AppComponent {
       
     }
     this.result = "";
+
+    if(this.display.includes(".")){
+      if(this.display.endsWith("0")){
+        this.favela();
+      }
+    }
+
+    
+
+
   }
+  // 0.1123000 sondaki sıfırları silme
+  favela():void{
+
+    for(let i=0;i<this.display.length;i++){
+      if(this.display.includes(".")){
+        if(this.display.endsWith("0")){
+          this.display = this.display.substring(0,this.display.length-1);
+        }
+        else{
+          break;
+        }
+      }
+    }
+
+
+    
+  }
+
+
+
 
   sqrt():void {
     if(this.result.includes("+")||this.result.includes("-")||this.result.includes("x")
